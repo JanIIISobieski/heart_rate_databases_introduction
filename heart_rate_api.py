9b7a04b6b670edf1427b8bd0b2f2e8ab2c1c3c3c
Gabriel Antoniak <gjantoniak@gmail.com>
2018-03-23 15:32:41 -0400

    First code completion of api calls

--- heart_rate_api.py
+++ heart_rate_api.py
@@ -1,57 +1,84 @@
 from flask import Flask, jsonify, request
-from pymodm import connect
 import models
 import datetime
 import main
 
 app = Flask(__name__)
 
+
 @app.route('/api/heart_rate', methods=['POST'])
 def post_heart_rate():
     r = request.get_json()
     print(r)
-    if is_subject_in_db(r):  #if subject already exists
+    if is_subject_in_db(r):  # if subject already exists
         main.add_heart_rate(r['user_email'],
                             r['heart_rate'],
-                            datetime.datetime.now())  
-        print('Added heart rate of ' + str(r['heart_rate']) + ' to ' + r['user_email'])
+                            datetime.datetime.now())
+        text = ''.join('Added heart rate of ', str(r['heart_rate']),
+                       ' to ', r['user_email'])
     else:
         main.create_user(r['user_email'],
                          r['user_age'],
                          r['heart_rate'],
                          datetime.datetime.now())
-        print('Created user ' + r['user_email'] +
-              ' and initialized with HR ' + r['heart_rate'])
-    return 
-        
+        text = ''.join('Created user ', r['user_email'],
+                       ' and initialized with HR ', str(r['heart_rate']))
+    return jsonify({'info': text})
+
+
 def is_subject_in_db(json_file):
-    user = models.User.objects.raw({"_id": json_file['user_email']}).first()
-    return user.email == json_file('user_email')
+    try:
+        models.User.objects.raw({"_id": json_file['user_email']}).first()
+        user_exists = True
+    except models.DoesNotExist:
+        user_exists = False
+    return user_exists
+
 
 @app.route('/api/heart_rate/<user_email>', methods=['GET'])
 def get_user_heart_rates(user_email):
-    wanted_user = models.User.objects.raw({'_id': user_email}).first()
-    return jsonify({'heart_rate': wanted_user.heart_rate})
+    if is_subject_in_db(user_email):
+        wanted_user = models.User.objects.raw({'_id': user_email}).first()
+        return jsonify({'heart_rate': wanted_user.heart_rate})
+    else:
+        return jsonify({'error': 'User not in database'})
+
 
 @app.route('/api/heart_rate/average/<user_email>', methods=['GET'])
 def get_avg_heart_rates(user_email):
     from numpy import mean
-    wanted_user = models.User.objects.raw({'_id': user_email}).first()
-    return jsonify({'avg_heart_rate': mean(wanted_user.heart_rate)})
+    if is_subject_in_db(user_email):
+        wanted_user = models.User.objects.raw({'_id': user_email}).first()
+        return jsonify({'avg_heart_rate': mean(wanted_user.heart_rate)})
+    else:
+        return jsonify({'error': 'User not in database'})
+
 
 @app.route('/api/heart_rate/interval_average', methods=['POST'])
 def get_int_average():
-    from numpy import array, greater
+    from numpy import array, mean
     r = request.get_json()
     print(r)
-    
-    datetime_format = '%Y-%m-%d %H:%M:%S:%f'
+
+    datetime_format = '%Y-%m-%d %H:%M:%S.%f'
     cutoff_date = datetime.datetime.strptime(r['heart_rate_average_since'],
                                              datetime_format)
-    
-    wanted_user = models.User.objects.raw({'_id': r['user_email']}).first()
-    
-    
+    if is_subject_in_db(r['user_email']):
+        wanted_user = models.User.objects.raw({'_id': r['user_email']}).first()
+        heart_times = array(wanted_user.heart_rate_times)
+        heart_rates = array(wanted_user.heart_rate)
+        mean_heart_rate = mean(heart_rates[heart_times > cutoff_date])
+
+        tachy_flag = is_tachycardic(wanted_user.age, mean_heart_rate)
+
+        return jsonify({'avg_heart_rate_since_date': mean_heart_rate,
+                        'tachycardic': tachy_flag})
+    else:
+        return jsonify({'error': 'User not in database'})
+
+
+def is_tachycardic(age, u_heart_rate):
+    from numpy import array
     tachy_dict = {0: 159,
                   3/365: 166,
                   7/365: 182,
@@ -63,10 +90,8 @@ def get_int_average():
                   5: 133,
                   8: 142,
                   12: 119,
-                  15: 100}  #lower age cutoffs for defining tachycardia
-    
+                  15: 100}  # lower age cutoffs for defining tachycardia
+
     tachy_keys = array(list(tachy_dict.keys()))
-    
-    pass
-    
-    
+    tachy_cutoff = tachy_dict[tachy_keys[tachy_keys < age][-1]]
+    return u_heart_rate > tachy_cutoff
